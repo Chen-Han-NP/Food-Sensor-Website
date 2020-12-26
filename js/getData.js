f092f51c65ec0c728dd531804a9fdc709f80f87d
Chen-Han-NP <s10202961@connect.np.edu.sg>
2020-12-26 15:27:44 +0800

    Created a map that display singapore as default

--- js/getData.js
+++ js/getData.js
@@ -1,6 +1,15 @@
+/*
 fetch('https://randomuser.me/api/')
     .then(response => response.json())
     .then(function (data){
         console.log(data.results[0].gender);
         $(`#content`).text(`\n${data.results[0].gender}`);
     });
+*/
+function initMap(){
+    var location = {lat: 1.352083, lng: 103.819839};
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 13,
+        center: location
+    });
+}

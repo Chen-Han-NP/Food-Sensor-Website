8e08318564b198861566f52d6f4cdcdea5b09356
Chen-Han-NP <s10202961@connect.np.edu.sg>
2020-12-28 14:04:09 +0800

    added some features to get the user's current location and display it on the screen

--- js/getData.js
+++ js/getData.js
@@ -6,6 +6,10 @@ fetch('https://randomuser.me/api/')
         $(`#content`).text(`\n${data.results[0].gender}`);
     });
 */
+let current_position = {
+  latitude: 0,
+  longitude: 0
+}
 
 function getMyLocation(){
     if (navigator.geolocation) {
@@ -17,8 +21,18 @@ function getMyLocation(){
 
 }
 function showPosition(position) {
-    setCoords(position.coords.latitude, position.coords.longitude, 18);
-    initMap();
-   console.log("Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude);
+  current_position.latitude = position.coords.latitude;
+  current_position.longitude = position.coords.longitude;
+  setCoords(position.coords.latitude, position.coords.longitude, 18);
+  initMap();
+  let url = 'https://maps.googleapis.com/maps/api/geocode/json?key=' + 'AIzaSyCHlRzdfgMvGjszjoE4zISUcUHDqXqup80&latlng=' + current_position.latitude + "," + current_position.longitude;
+  fetch(url)
+    .then(response => response.json())
+    .then(function (data){
+      //console.log(data)
+      $(`#location`).text(data.results[0]['formatted_address']);
+    });
+  
+  console.log("Latitude: " + current_position.latitude + 
+    "<br>Longitude: " + current_position.longitude);
   }

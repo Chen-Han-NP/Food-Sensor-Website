71114a87167cc3991587765848308f3538c5232d
Chen-Han-NP <s10202961@connect.np.edu.sg>
2020-12-28 15:48:50 +0800

    user now can enter their location manually

--- js/displayMap.js
+++ js/displayMap.js
@@ -1,8 +1,9 @@
+
 var latt = 1.352083;
 var lngg = 103.819839;
 var zooms = 12;
 
-function setCoords(latt,lngg, zooms){
+function setCoords(latt, lngg, zooms){
   this.latt = latt;
   this.lngg = lngg;
   this.zooms = zooms;
@@ -18,8 +19,63 @@ function initMap(){
         position: location,
         map: map,
       });
+    
+    
+    var input = document.getElementById('my-input-searchbox');
+    const searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener("bounds_changed", () => {
+      searchBox.setBounds(map.getBounds());
+    });
+    let markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener("places_changed", () => {
+      const places = searchBox.getPlaces();
+  
+      if (places.length == 0) {
+        return;
+      }
+      // Clear out the old markers.
+      markers.forEach((marker) => {
+        marker.setMap(null);
+      });
+      markers = [];
+      // For each place, get the icon, name and location.
+      const bounds = new google.maps.LatLngBounds();
+      places.forEach((place) => {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        const icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25),
+        };
+        // Create a marker for each place.
+        markers.push(
+          new google.maps.Marker({
+            map,
+            icon,
+            title: place.name,
+            position: place.geometry.location,
+          })
+        );
+  
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
 }
-
 /*
 let map, infoWindow;
 
